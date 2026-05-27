9f6b932538ff5b5227ce3f98f68d2694c8c0a14e
Okeke Kenneth <acmedigital4ken@gmail.com>
2026-05-28 00:53:12 +0100

    Fix print script tag ESLint error

--- src/App.js
+++ src/App.js
@@ -326,7 +326,8 @@ function MemberPayment({ member, showToast }) {
     const el = receiptRef.current;
     if (!el) return;
     const w = window.open("", "_blank");
-    w.document.write("<html><head><title>PHConnectz Receipt</title></head><body>" + el.outerHTML + "<script>window.print();window.close();" + "<" + "/script></body></html>");
+    const scriptTag = "<script>window.print();window.close();</s" + "cript>";
+    w.document.write("<html><head><title>PHConnectz Receipt</title></head><body>" + el.outerHTML + scriptTag + "</body></html>");
     w.document.close();
   };
 
